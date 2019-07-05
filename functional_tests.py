0a37c2444bef1a58635d19a89b8dd71c1b89866f
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-07-05 20:58:48 +0430

    test refactored

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        item_list = [row.text for row in rows]
+        self.assertIn(
+            row_text, item_list,
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        )
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -39,32 +48,18 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Buy peacock feathers', [row.text for row in rows],
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_table_list('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys("Use peacock feathers to make a fly")
         input_box.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        current_list = [row.text for row in rows]
-        self.assertIn(
-            '1: Buy peacock feathers', current_list,
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly", current_list,
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_table_list('1: Buy peacock feathers')
+        self.check_for_row_in_table_list('2: Use peacock feathers to make a fly')
 
         self.fail('finish the test')
 
