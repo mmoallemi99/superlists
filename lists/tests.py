ff23411b8d071186136af4fa66174e9ad2001b90
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-07-05 21:32:16 +0430

    written model test and model itself

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 
+from .models import Item
 
-class FailingTest(TestCase):
+
+class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
@@ -13,4 +15,22 @@ class FailingTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
+class ItemModelTest(TestCase):
 
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
