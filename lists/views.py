dc77e1bb2a3973e85d3de2351b9482aa1a62d444
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-07-11 01:05:54 +0430

    more unit tests

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Item
+from .models import Item, List
 
 
 def home_page(request):
@@ -16,6 +16,7 @@ def view_list(request):
 
 
 def new_list(request):
+    list_ = List.objects.create()
     new_item_text = request.POST['item_text']
-    Item.objects.create(text=new_item_text)
+    Item.objects.create(text=new_item_text, list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
