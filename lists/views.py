dc4b85ffca0504c4be1824939dafa6316a685337
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-07-10 22:53:48 +0430

    some new tests and refactoring and url change

--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,6 @@ from .models import Item
 
 
 def home_page(request):
-
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 
@@ -19,3 +13,9 @@ def view_list(request):
         'items': items,
     }
     return render(request, 'list.html', context)
+
+
+def new_list(request):
+    new_item_text = request.POST['item_text']
+    Item.objects.create(text=new_item_text)
+    return redirect('/lists/the-only-list-in-the-world/')
